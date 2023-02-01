65213b916ed2765304042fbd8ffa2ff17b9cbf9a
evanhutagaol <evanhutagaol15@gmail.com>
2023-02-01 21:48:58 +0700

    update AppServiceProvider

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,30 +1,44 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> 
         @yield('title') | Fabcart
     </title>
-    <!-- Bootsrap core CSS -->
-    <link href="{{ asset('assets/css/bootsrap.min.css') }}" rel="stylesgeet">
-    <!-- Material Design Bootsrap -->
-    <link href="{{ asset('assets/css/mdb.min.css')}}" rel="stylesgeet">
-    <!-- Your custom styles (optional) -->
-    <link href="{{ asset('assets/css/style.css')}}" rel="stylesgeet">
+    {{-- font awesome --}}
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    {{-- bootstrap core css --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    {{-- material design bootstrap --}}
+    <link rel="stylesheet" href="{{ asset('/assets/css/mdb.min.css') }}">
+    {{-- your custom style --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
+
 <body>
+
     @include('layouts.inc.front-navbar')
 
+    <main>
+        @yield('content')
+    </main>
 
+    @include('layouts.inc.front-footer')
 
-    <!-- JQuery -->
-    <script type="text/javascript" src="{{ asset(''assets/js/jquery.min.js) }}"></script>
-    <script type="text/javascript" src="{{ asset(''assets/js/popper.min.js) }}"></script>
-    <script type="text/javascript" src="{{ asset(''assets/js/bootsrap.min.js) }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/js/mdb.min.js') }}"></script>
 
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+    <!-- Plugin file -->
+    <script src="./js/addons/datatables.min.js"></script>
 </body>
+
 </html>
