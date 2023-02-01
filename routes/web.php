4b35ff42dbfcd9eee47affa0ea915d9d8fa7c95c
evanhutagaol <evanhutagaol15@gmail.com>
2023-02-01 22:27:06 +0700

    create authentication

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,15 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
