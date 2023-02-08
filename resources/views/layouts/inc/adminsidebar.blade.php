7c1c8080878f05dc38ee5660c6b031aa329ce41c
evanhutagaol <evanhutagaol15@gmail.com>
2023-02-08 21:45:22 +0700

    initial commit

--- resources/views/layouts/inc/adminsidebar.blade.php
+++ resources/views/layouts/inc/adminsidebar.blade.php
@@ -6,17 +6,17 @@
     </a>
 
     <div class="list-group list-group-flush">
-      <a href="#" class="list-group-item active waves-effect">
-        <i class="fas fa-chart-pie mr-3"></i>Dashboard
-      </a>
-      <a href="#" class="list-group-item list-group-item-action waves-effect">
-        <i class="fas fa-user mr-3"></i>Profile</a>
-      <a href="#" class="list-group-item list-group-item-action waves-effect">
-        <i class="fas fa-table mr-3"></i>Tables</a>
-      <a href="#" class="list-group-item list-group-item-action waves-effect">
-        <i class="fas fa-map mr-3"></i>Maps</a>
-      <a href="#" class="list-group-item list-group-item-action waves-effect">
-        <i class="fas fa-money-bill-alt mr-3"></i>Orders</a>
+        <a href="#" class="list-group-item active waves-effect">
+            <i class="fas fa-chart-pie mr-3"></i>Dashboard
+        </a>
+        <a href="#" class="list-group-item list-group-item-action waves-effect">
+            <i class="fas fa-user mr-3"></i>Profile</a>
+        <a href="#" class="list-group-item list-group-item-action waves-effect">
+            <i class="fas fa-table mr-3"></i>Tables</a>
+        <a href="#" class="list-group-item list-group-item-action waves-effect">
+            <i class="fas fa-map mr-3"></i>Maps</a>
+        <a href="{{ url('registered-user') }}" class="list-group-item list-group-item-action waves-effect">
+            <i class="fas fa-users mr-3"></i>Registered User</a>
     </div>
 
   </div>
